f01663e766672c8b9dd5a5cd77691d59caa8a9ab
毛毅晗 <yihan.mao@suanzi.cn>
2016-03-04 13:30:12 +0800

    使用universalImageLoad加载网络图片

--- app/src/main/java/com/example/maoyh/myapplication/app/adpter/ListviewItemAdpter.java
+++ app/src/main/java/com/example/maoyh/myapplication/app/adpter/ListviewItemAdpter.java
@@ -10,6 +10,11 @@ import android.widget.TextView;
 
 import com.example.maoyh.myapplication.app.R;
 import com.example.maoyh.myapplication.app.entity.ListEntity;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
 
 import java.util.List;
 
@@ -20,13 +25,23 @@ import java.util.List;
 public class ListviewItemAdpter extends BaseAdapter {
     private List<ListEntity> data;
     private LayoutInflater layoutInflater;
-    private Context context;
+    private ImageLoader mImageLoader = ImageLoader.getInstance();
+    private DisplayImageOptions options;
+    private Context mContext;
+
 
     public ListviewItemAdpter(Context context, List<ListEntity> data) {
         this.data = data;
-
-        this.context = context;
+        this. mContext = context;
         this.layoutInflater = layoutInflater.from(context);
+        mImageLoader.init(ImageLoaderConfiguration.createDefault(context));
+        options = new DisplayImageOptions.Builder()
+                .showImageForEmptyUri(R.mipmap.ic_launcher)
+                .showImageOnFail(R.mipmap.ic_launcher).cacheInMemory()
+                .displayer(new RoundedBitmapDisplayer(20))
+                .displayer(new FadeInBitmapDisplayer(300))
+                .build();
+
     }
 
     @Override
@@ -57,14 +72,15 @@ public class ListviewItemAdpter extends BaseAdapter {
         } else {
             viewHolder = (ViewHolder) view.getTag();
         }
-        viewHolder.Icon.setBackgroundResource(data.get(i).getIcon());
+        //viewHolder.Icon.setBackgroundResource(data.get(i).getIcon());
+
+        mImageLoader.displayImage("http://img4.imgtn.bdimg.com/it/u=1728896325,3327684983&fm=21&gp=0.jpg",viewHolder.Icon,options);
         viewHolder.Title.setText(data.get(i).getTitle());
         viewHolder.Content.setText(data.get(i).getContent());
 
 
         return view;
     }
-
     private static class ViewHolder {
 
         ImageView Icon;
